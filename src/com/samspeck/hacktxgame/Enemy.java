847a44163475052aeae07c09968969b62067ec39
Po-Chen <pc.yang@utexas.edu>
2013-11-16 00:02:39 -0600

    Added enemy.java

---
+++ src/com/samspeck/hacktxgame/Enemy.java
@@ -0,0 +1,26 @@
+package com.samspeck.hacktxgame;
+
+public class Enemy extends Entity {
+	// movement pattern
+		//left right turn around on obstacle (air or land)
+		//up down turn around on obstacle (air or land)
+		//move towards the character but don't turn around
+		//follow the character and turn around
+		//stand in place
+		//jump
+	// die
+		// being stepped on
+		// fell off of the map
+		// being hit by the player's ability
+		// invincible
+		// Suicide attack
+	// attack
+		// touch the player
+		// throw item at the player
+		// spawn another enemy
+	// dodge player's attack
+		// instantly move one direction when player approach from above
+	// mutate?
+	// sounds?
+	
+}
